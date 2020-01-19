484819401bb29d30f667e198693ab1d8b82326d3
Winter4T2H <silverstar42.www.ce@gmail.com>
2020-01-19 16:37:00 +0800

    最終移動雛形(包括空中慣性)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -25,7 +25,11 @@ public class PlayerMovement : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
+    public float x;
+    public float z;
+
     Vector3 velocity;
+    Vector3 SpeedCheck;
     bool isGrounded;
 
 
@@ -34,25 +38,51 @@ public class PlayerMovement : MonoBehaviour
     //                                 //
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        SpeedCheck.y = velocity.y;
+        SpeedCheck.z = z * 250;
+        SpeedCheck.x = x * 250;
 
+
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        OnIdle();
+        GravityF();
+        if (Input.GetButton("Jump"))
+        {
+            velocity = transform.right * x * speed + transform.forward * z * speed;
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity * 2);
+        }
+        if (Input.GetButton("Fire3"))
+        {
+            speed = 24f;
+        }
+        else
+        {
+            speed = 12f;
+        }
+    }
+
+    void GravityF()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         if (isGrounded && velocity.y < 0)
         {
+            velocity.z = 0f;
+            velocity.x = 0f;
             velocity.y = -2f;
         }
-
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+    }
+
+
+
+    void OnIdle()
+    {
+        if (isGrounded == true)
+        {
+            Vector3 move = transform.right * x + transform.forward * z;
+            controller.Move(move * speed * Time.deltaTime);
+        }
     }
 }
