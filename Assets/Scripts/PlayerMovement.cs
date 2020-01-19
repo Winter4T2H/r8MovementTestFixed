9e75f385cf2c46bcc901580f730552dd7dffe64b
Winter4T2H <silverstar42.www.ce@gmail.com>
2020-01-19 14:50:42 +0800

    wwwwwwwwwww

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,7 +11,6 @@ using UnityEngine;
  * 
  * Warning: This is a re-write from Brackeys' default scrips.
  * 
- * 
  */
 
 public class PlayerMovement : MonoBehaviour
@@ -30,56 +29,30 @@ public class PlayerMovement : MonoBehaviour
     bool isGrounded;
 
 
-
-    private void Start()
-    {
-        Thread th_OnIdle = new Thread(OnIdle);
-        th_OnIdle.Start();
-    }
     //                                 //
     // Update is called once per frame //
     //                                 //
     void Update()
     {
-        // Empty
-    }
-
-    void OnIdle()
-    {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        for (float i = 2; i < 1; i++)
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            i = 0;
-            if (Input.GetButtonDown("Jump") && isGrounded)
-            {
-                OnJump();
-            }
-            else
-            {
-                if (isGrounded && velocity.y < 0)
-                {
-                    velocity.y = -2f;
-                }
-                Vector3 move = transform.right * x + transform.forward * z;
-                controller.Move(move * speed * Time.deltaTime);
-                velocity.y += gravity * Time.deltaTime;
-                controller.Move(velocity * Time.deltaTime);
-            }
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
-    }
-    void OnJump()
-    {
-        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        for (int i = 0; isGrounded == false; i++)
-        {
-            if (isGrounded)
-            {
-                break;
-            }
-        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
